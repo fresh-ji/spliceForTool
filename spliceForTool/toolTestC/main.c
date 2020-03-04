9701f619b42504740a12655c9a6bb4823743751b
huangxuesong <huangxuesong@cqhorizon.com>
2020-03-04 11:08:30 +0800

    1、添加时间管理，仿真控制库； 2、添加controlTest测试程序； 3、修改toolTestA，toolTestB，toolTestC
    Signed-off-by: huangxuesong <huangxuesong@cqhorizon.com>

--- spliceForTool/toolTestC/main.c
+++ spliceForTool/toolTestC/main.c
@@ -27,7 +27,7 @@ typedef struct _UDPosture {
 	int	gamma;
 }UDPosture;
 
-static double topic_data = 0;
+static UDPosture posture;
 
 void initTool(double, double);
 void setToTool(double, char*, void*);
@@ -70,24 +70,26 @@ void setToTool(double time, char* name, void* data) {
 		printf("y : %f\n", pos->y);
 		printf("z : %f\n", pos->z);
 	}
-	else if (strcmp(name, "topic_003") == 0)
+	else if (strcmp(name, "topic_001") == 0)
 	{
-		UDPosture* pos = (UDPosture*)data;
-		printf("info:\n");
-		printf("vx : %d\n", pos->vx);
-		printf("vy : %d\n", pos->vy);
-		printf("vz : %d\n", pos->vz);
-		printf("phi : %d\n", pos->phi);
-		printf("psi : %d\n", pos->psi);
-		printf("gamma : %d\n", pos->gamma);
+		double d = *(double*)data;
+		printf("topic_001 : %f\n", d);
 	}
 
 	//setFinish(time);
 }
 
 void setFinish(double time) {
-	topic_data += 1;
-	setFun("topic_001", (void*)&topic_data);
+
+	posture.vx = posture.vx + 100;
+	posture.vy = posture.vy + 100;
+	posture.vz = posture.vz + 100;
+	posture.psi = posture.psi + 100;
+	posture.phi = posture.phi + 100;
+	posture.gamma = posture.gamma + 100;
+
+	setFun("topic_003", (void*)&posture);
+
 	printf("i did something and go forward to %f\n", time);
 	advanceFun();
 }
@@ -130,7 +132,7 @@ int main(int argc, char *argv[]) {
 	advanceFun = (FunDLL3)GetProcAddress(hInstC, "dllAdvance");
 	endFun = (FunDLL4)GetProcAddress(hInstC, "dllEnd");
 
-	startFun("E:\\2y\\spliceForTool\\ZtOE0Jfu_insA.xml",
+	startFun("E:\\2y\\spliceForTool\\ZtOE0Jfu_insC.xml",
 		initTool, setToTool, setFinish, endTool);
 
 	/*while (1) {
@@ -141,26 +143,26 @@ int main(int argc, char *argv[]) {
 		}
 	}*/
 
-	UDPosition pos;
-	pos.longitude = 0.0;
-	pos.latitude = 0.0;
-	pos.altitude = 0.0;
-	pos.x = 0.0;
-	pos.y = 0.0;
-	pos.z = 0.0;
-	char topic_name[10] = "topic_002";
+	
+	posture.vx = 100;
+	posture.vy = 100;
+	posture.vz = 100;
+	posture.psi = 100;
+	posture.phi = 100;
+	posture.gamma= 100;
+	/*char topic_name[10] = "topic_003";
 	char topic_name2[20] = "advance_grant";
 	int time = 1;
-	/*while (1)
+	while (1)
 	{
-		pos.longitude = pos.longitude + 1;
-		pos.latitude = pos.latitude + 1;
-		pos.altitude = pos.altitude + 1;
-		pos.x = pos.x + 1;
-		pos.y = pos.y + 1;
-		pos.z = pos.z + 1;
+		posture.vx = posture.vx + 100;
+		posture.vy = posture.vy + 100;
+		posture.vz = posture.vz + 100;
+		posture.psi = posture.psi + 100;
+		posture.phi = posture.phi + 100;
+		posture.gamma = posture.gamma + 100;
 
-		setFun(topic_name, (void*)&pos);
+		setFun(topic_name, (void*)&posture);
 		if (time % 3 == 0)
 		{
 			setFun(topic_name2, (void*)NULL);
@@ -173,9 +175,3 @@ int main(int argc, char *argv[]) {
 	getchar();
 	return 1;
 }
-
-
-
-
-
-
