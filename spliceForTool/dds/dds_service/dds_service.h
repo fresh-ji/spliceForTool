bc350203d89b0a0fc77a54a6391bfddb1d866b08
huangxuesong <huangxuesong@cqhorizon.com>
2020-03-23 14:57:33 +0800

    修改dds接口类，根据主题名称创建/获取 主题，读者，写者等； 读数据线程添加标志位，用于停止线程；
    Signed-off-by: huangxuesong <huangxuesong@cqhorizon.com>

--- spliceForTool/dds/dds_service/dds_service.h
+++ spliceForTool/dds/dds_service/dds_service.h
@@ -8,6 +8,9 @@
 //std
 #include <vector>
 #include <functional>
+#include <map>
+#include <thread>
+#include <atomic>
 
 using namespace DDS;
 
@@ -36,12 +39,12 @@ class CSDDS_SERVICE_EXPORT CSDDSService  {
   bool CreatePublisher();
   bool CreateSubscriber();
   
-  bool CreateWriter();
-  bool CreateReader();
+  bool CreateWriter(const std::string& topic_name);
+  bool CreateReader(const std::string& topic_name);
   
-  bool write(const MsgData& msg_data);
-  std::vector<MsgData> read();
-  std::vector<MsgData> take();
+  bool write(const std::string &topic_name, const MsgData& msg_data);
+  std::vector<MsgData> read(const std::string &topic_name);
+  std::vector<MsgData> take(const std::string &topic_name);
 
   void ReadWithWaitSet();
 
@@ -50,11 +53,11 @@ class CSDDS_SERVICE_EXPORT CSDDSService  {
   void SetCallBack(std::function<bool(MsgData)>);
   //void SetCallBack(void *cb);
 
-  DataReader_ptr getReader();
-  DataWriter_ptr getWriter();
+  DataReader_ptr getReader(const std::string& topic_name);
+  DataWriter_ptr getWriter(const std::string& topic_name);
   Publisher_ptr getPublisher();
   Subscriber_ptr getSubscriber();
-  Topic_ptr getTopic();
+  Topic_ptr getTopic(const std::string &topic_name);
   DomainParticipant_ptr getParticipant();
 
   CSDDSService();
@@ -69,19 +72,29 @@ class CSDDS_SERVICE_EXPORT CSDDSService  {
   DDS::String_var partition_name_;
   DDS::String_var type_name_;
 
-  Topic_var topic_;
+  //Topic_var topic_;
 
   Publisher_var publisher_;
   Subscriber_var subscriber_;
 
-  DataWriter_var writer_;
-  DataReader_var reader_;
+  //DataWriter_var writer_;
+  //DataReader_var reader_;
 
-  ReadCondition_var newMsg;
-  WaitSet_var newMsgWS;
+  //ReadCondition_var newMsg;
+  //WaitSet_var newMsgWS;
   Duration_t wait_timeout;
 
   std::function<bool(MsgData)> cb_;
+  
+  std::thread read_thread_;
+  std::atomic<bool> read_flag_ = true;
+
+  std::map<std::string,Topic_var> topics_;
+  std::map<std::string, DataReader_var> readers_;
+  std::map<std::string, DataWriter_var> writers_;
+
+  std::map<std::string, ReadCondition_var> conditions_;
+  std::map<std::string, WaitSet_var>waitsets_;
   //void *cb_;
 };
 
